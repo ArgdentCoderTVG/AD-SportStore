f1bb81de5a7088206724537837e5fc7b7c9e7171
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-11-10 13:12:19 +0100

    Added Category and Product Entities

---
+++ SportStore/Data/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace SportStore.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation property
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+}
---
+++ SportStore/Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace SportStore.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        // Navigation property
+        public virtual Category Category { get; set; }
+    }
+
+}
