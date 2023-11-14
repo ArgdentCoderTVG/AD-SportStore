c2528d940f53cb1cd5714e14222969af9f776f61
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-11-14 15:52:57 +0100

    Refactored ents Category and Product (better Convention+Annotation+FluentAPI) synergy

--- SportStore/Data/Entities/Category.cs
+++ SportStore/Data/Entities/Category.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportStore.Data.Entities
 {
@@ -8,10 +7,9 @@ namespace SportStore.Data.Entities
         // Constants, member variables
         const byte MAXLENGTH_FOR_STRINGS = 100;
 
-        [Key]
         public int Id { get; set; }
         
-        [Column, Required, MaxLength(MAXLENGTH_FOR_STRINGS)]
+        [MaxLength(MAXLENGTH_FOR_STRINGS)]
         public string Name { get; set; }
 
         public ICollection<Product> Products { get; set; }
--- SportStore/Data/Entities/Product.cs
+++ SportStore/Data/Entities/Product.cs
@@ -1,6 +1,4 @@
-﻿using SportStore.Data.Entities;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SportStore.Data.Entities
 {
@@ -9,17 +7,14 @@ namespace SportStore.Data.Entities
         // Constants, member variables
         const byte MAXLENGTH_FOR_STRINGS = 100;
         
-        [Key]
         public int Id { get; set; } = default;
-        [Required, MaxLength(MAXLENGTH_FOR_STRINGS)]
+        [MaxLength(MAXLENGTH_FOR_STRINGS)]
         public string Name { get; set; } = String.Empty;
         [MaxLength(MAXLENGTH_FOR_STRINGS)]
         public string? Description { get; set; } = default;
-        [Required]
         public decimal Price { get; set; } = default;
         public string? ImageURL { get; set; } = default;
 
-        [ForeignKey("CategoryFK")]
         public int? CategoryFK { get; set; } = default;
         public Category? Category { get; set; } = default;
     }
